ff3b141c4847aa870f11b4382a8345921bdc81db
Jysan Aziz <jeexan2@gmail.com>
2018-12-19 19:48:43 +0600

    afs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Design_Pattern_Project
 {
     class Program
     {
-    
+    //ckmm
         static void Main(string[] args)
         {
             POCO_CLASSES pOCO = new POCO_CLASSES();
